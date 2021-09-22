60de8a0ca199a4e02f4ff22cefaf96fb3bb5cf0a
Freddy Suarez <freddy.armando.suarez@gmail.com>
2021-09-22 19:34:38 -0300

    initial commit

---
+++ src/test/java/demotest/Util.java
@@ -0,0 +1,35 @@
+package demotest;
+
+public class Util {
+	private String driverLocation;
+	private String baseUrl;
+	private String username;
+	private String password;
+	
+	
+	public String getDriverLocation() {
+		return driverLocation;
+	}
+	public void setDriverLocation(String driverLocation) {
+		this.driverLocation = driverLocation;
+	}
+	public String getBaseUrl() {
+		return baseUrl;
+	}
+	public void setBaseUrl(String baseUrl) {
+		this.baseUrl = baseUrl;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
